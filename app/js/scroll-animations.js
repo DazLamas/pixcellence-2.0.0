8d9b0a91eaca734b54e8c4a938f9e798010ee38c
DazLamas <sarah.d.lamas@gmail.com>
2019-06-12 17:46:46 +0200

    Remove bg animation on scroll

--- app/js/scroll-animations.js
+++ app/js/scroll-animations.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-
-  $(window).on('scroll', function() {
-
-    var window_height = $(window).height();
-    var bottom_limit  = window_height * .9;
-    var $element = $('#change-bg');
-
-    $element.each(function() {
-
-      var element_distance_from_top = $(this).offset().top;
-      var scroll_from_top           = $(window).scrollTop();
-      var element_top = element_distance_from_top - scroll_from_top;
-
-      if ((element_top + $(this).height()) < bottom_limit) {
-        // $(this).css('background-color', 'black');
-        $(this).addClass('overlay-bg');
-        console.log('está');
-      } else {
-        $(this).removeClass('overlay-bg');
-        console.log('NO');
-      }
-    });
-
-  });
-
-  // $(window).trigger('scroll');
-
-});
+// $(document).ready(function() {
+//
+//   $(window).on('scroll', function() {
+//
+//     var window_height = $(window).height();
+//     var bottom_limit  = window_height * .9;
+//     var $element = $('#change-bg');
+//
+//     $element.each(function() {
+//
+//       var element_distance_from_top = $(this).offset().top;
+//       var scroll_from_top           = $(window).scrollTop();
+//       var element_top = element_distance_from_top - scroll_from_top;
+//
+//       if ((element_top + $(this).height()) < bottom_limit) {
+//         // $(this).css('background-color', 'black');
+//         $(this).addClass('overlay-bg');
+//         console.log('está');
+//       } else {
+//         $(this).removeClass('overlay-bg');
+//         console.log('NO');
+//       }
+//     });
+//
+//   });
+//
+//   // $(window).trigger('scroll');
+//
+// });
